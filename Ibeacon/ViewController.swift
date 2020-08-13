341b8ae850351541f3cffa4ef0d9858e9d897de2
ed1868 <ruizeduardo21@gmail.com>
2020-08-13 15:02:56 -0400

    Initial Commit

---
+++ Ibeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ibeacon
+//
+//  Created by Eddie Ruiz on 8/13/20.
+//  Copyright © 2020 AI Nomads. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
